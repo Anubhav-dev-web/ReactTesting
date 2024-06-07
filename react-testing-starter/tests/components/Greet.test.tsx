e8a4e7823963f37621182b2b14daa53b19bdc06d
Anubhav <lucimgstar007@gmail.com>
2024-06-07 21:17:15 +0530

    found a bug

--- react-testing-starter/tests/components/Greet.test.tsx
+++ react-testing-starter/tests/components/Greet.test.tsx
@@ -1,9 +1,13 @@
 import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Greet from "../../src/components/Greet";
+import Greet from "../../components/Greet";
+import "@testing-library/jest-dom/vitest";
 
 describe("Greet", () => {
   it("should render Hello with the name when the name is provided", () => {
     render(<Greet name="John" />);
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toBeInTheDocument();
   });
 });
