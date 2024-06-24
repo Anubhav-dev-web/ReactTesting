6cb89f69f1d60b0a3d2605545f923a83e447ed81
Anubhav <lucimgstar007@gmail.com>
2024-06-24 17:04:18 +0530

    Testing pahse 1 completed

--- react-testing-starter/src/pages/PlaygroundPage.tsx
+++ react-testing-starter/src/pages/PlaygroundPage.tsx
@@ -1,7 +1,12 @@
-import Onboarding from "../components/Onboarding";
+// import Onboarding from "../components/Onboarding";
+// import TermsAndConditions from "../components/TermsAndConditions";
+
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+  return (
+    <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro incidunt recusandae voluptatem maiores natus aliquid non. Sit itaque perferendis assumenda quis id autem provident voluptate reprehenderit, nisi eaque in animi consequatur tempore voluptas? Qui nostrum quo suscipit eos iure ab dolorem quos nemo. Natus ipsam repellat praesentium similique recusandae eaque animi ipsa sint excepturi eum repellendus facilis suscipit tenetur error ab non debitis, omnis nam illo sequi ea! Alias omnis itaque odio dignissimos totam ab officiis consequuntur voluptas eligendi amet accusamus hic officia iste fugiat deserunt, placeat natus ut, fuga adipisci et? Necessitatibus eligendi in cupiditate! Tempore iure dolorem soluta!" />
+  );
 };
 
 export default PlaygroundPage;
